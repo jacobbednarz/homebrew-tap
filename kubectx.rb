6607c859b06edf1dddb981f1fb07bb3259d65657
Jacob Bednarz <jacob.bednarz@gmail.com>
2021-02-24 14:56:38 +1100

    Update kubectx.rb

--- kubectx.rb
+++ kubectx.rb
@@ -1,6 +1,5 @@
 class Kubectx < Formula
-  desc ""
-  homepage ""
+  homepage "https://github.com/ahmetb/kubectx"
   url "https://github.com/ahmetb/kubectx/releases/download/v0.9.1/kubectx_v0.9.1_darwin_x86_64.tar.gz"
   sha256 "a46c5cf1f6ba8f6aa4dc75fc5a7086d294a28a19cf2fbbf18ac54108a7f713e1"
   license ""
