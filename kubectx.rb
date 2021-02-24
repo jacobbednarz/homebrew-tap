dd888ba46add9ef14998ccf0dfb9a381c279df85
Jacob Bednarz <jacob.bednarz@gmail.com>
2021-02-24 14:54:56 +1100

    Create kubectx.rb
    Upstream hasn't yet made the jump to the Go binaries

---
+++ kubectx.rb
@@ -0,0 +1,15 @@
+class Kubectx < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/ahmetb/kubectx/releases/download/v0.9.1/kubectx_v0.9.1_darwin_x86_64.tar.gz"
+  sha256 "a46c5cf1f6ba8f6aa4dc75fc5a7086d294a28a19cf2fbbf18ac54108a7f713e1"
+  license ""
+
+  def install
+    bin.install "kubectx"
+  end
+
+  test do
+    assert_match "USAGE:", shell_output("#{bin}/kubectx -h 2>&1")
+  end
+end
