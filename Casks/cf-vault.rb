3a0cecaf41ce11248a62d05bd7ca9cab4e6f0bb5
Jacob Bednarz <jacob.bednarz@hey.com>
2022-09-12 12:06:10 +1000

    Bump cf-vault to 0.0.13

--- Casks/cf-vault.rb
+++ Casks/cf-vault.rb
@@ -1,8 +1,8 @@
 cask "cf-vault" do
-  version "0.0.12"
-  sha256 "ed38835493086761d6a8b05702c00d38e6ce16cf19cd79379ff072ab50197cfa"
+  version "0.0.13"
+  sha256 "9e8fa7254f1234685dba4077ede099bac31d604df7cc8c20c0e4231383ec8c77"
 
-  url "https://github.com/jacobbednarz/cf-vault/releases/download/0.0.12/cf-vault_0.0.12_darwin_amd64.zip"
+  url "https://github.com/jacobbednarz/cf-vault/releases/download/0.0.13/cf-vault_0.0.13_darwin_amd64.zip"
   appcast "https://github.com/jacobbednarz/cf-vault/releases.atom"
   name "cf-vault"
   homepage "https://github.com/jacobbednarz/cf-vault"
