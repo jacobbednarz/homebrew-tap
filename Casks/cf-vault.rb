e3b58aa8623d0ac6c809b58da0614b06bd1db311
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-06-27 12:39:40 +1000

    make formula multi-arch

--- Casks/cf-vault.rb
+++ Casks/cf-vault.rb
@@ -1,8 +1,12 @@
 cask "cf-vault" do
   version "0.0.14"
-  sha256 "10cf88b62b0b9bb9a1d902275ac438fd23f4a7bf84ac85280dd877aaad7e60fb"
+  arch arm: "arm64", 
+       intel: "amd64"
 
-  url "https://github.com/jacobbednarz/cf-vault/releases/download/0.0.14/cf-vault_0.0.14_darwin_amd64.zip"
+  sha256 arm:   "419d9184e4a91b27e5a8330b47ca76bc05ccc6f2e236d06391e4a3b80e68d804",
+         intel: "10cf88b62b0b9bb9a1d902275ac438fd23f4a7bf84ac85280dd877aaad7e60fb"
+  
+  url "https://github.com/jacobbednarz/cf-vault/releases/download/#{version}/cf-vault_#{version}_darwin_#{arch}.zip"
   appcast "https://github.com/jacobbednarz/cf-vault/releases.atom"
   name "cf-vault"
   homepage "https://github.com/jacobbednarz/cf-vault"
