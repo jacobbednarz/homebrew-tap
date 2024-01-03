dae0c628b04a90bafbe17dd93c3d4f5558d2bfd8
Jacob Bednarz <jacob.bednarz@hey.com>
2024-01-04 08:21:47 +1100

    Updates cf-vault to 0.0.18

--- cf-vault.rb
+++ cf-vault.rb
@@ -1,8 +1,8 @@
 class CfVault < Formula
   desc "Manage your Cloudflare credentials, securely"
   homepage "https://github.com/jacobbednarz/cf-vault"
-  url "https://github.com/jacobbednarz/cf-vault/archive/refs/tags/0.0.17.tar.gz"
-  sha256 "c4df58aa61f6e7ca498e1ca49583e1f274fe9c88e60ec7a67bc9186e3b53ca66"
+  url "https://github.com/jacobbednarz/cf-vault/archive/refs/tags/0.0.18.tar.gz"
+  sha256 "cdb8eddfec7a153e2e75f42e8246f3f266ff3c566a5784f8d22b0a6b8f48c779"
   license "MIT"
   head "https://github.com/jacobbednarz/cf-vault.git", branch: "master"
 
