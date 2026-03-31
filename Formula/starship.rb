b52ff5a960a48822ac19e714f150447a723b4353
Jacob Bednarz <jacob.bednarz@gmail.com>
2026-03-31 14:45:58 +1100

    remove bottles

--- Formula/starship.rb
+++ Formula/starship.rb
@@ -6,16 +6,6 @@ class Starship < Formula
   license "ISC"
   head "https://github.com/jacobbednarz/starship.git", branch: "make-aws-support-patterns"
 
-  bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "261c37914c6e39b9f05633501ae38c7c44d0784a4886941a23f15bd6c49f7faa"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "afffc7a2bf021cac23ee10af2cee207ac2e16d193425935a34d3f58eb276da52"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d1bb0af009b2965ec7b6dd8410f068c90a7ba4c017b71243613185a7517c4540"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b2751c138704236385e82030494728e08b839fa5935a38aab40a8bcc5ae9ca31"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1ca52b67cf4b6f3004cbb3977d11923787ec17763cff6f448a339ac5643c1395"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "493d58232e62a9ae7ef86d039d442b7463495a907ffe14cb795cc590fadee3cb"
-  end
-
   depends_on "cmake" => :build
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
